1036e5437b5e194c5bfa41268607e60bcb137c5b
alexlockhart7 <alex.lockhart@icfolson.com>
2018-03-05 10:50:23 -0500

    Init commite.

---
+++ talis-core/src/main/groovy/com/talis/annotations/AEMComponent.groovy
@@ -0,0 +1,12 @@
+package com.talis.annotations
+
+import groovy.transform.AnnotationCollector
+import com.citytechinc.cq.component.annotations.Component
+import org.apache.sling.api.resource.Resource
+import org.apache.sling.models.annotations.Model
+
+@AnnotationCollector
+@Component(group = "")
+@Model(adaptables = Resource)
+@interface AEMComponent{
+}
