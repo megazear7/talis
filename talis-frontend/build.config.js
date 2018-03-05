b6c08e9f9656d29ddd39d73c30719dad93a1f24a
alexlockhart7 <alex.lockhart@icfolson.com>
2018-03-05 11:29:51 -0500

    Using webpack

--- talis-frontend/build.config.js
+++ talis-frontend/build.config.js
@@ -1,8 +1,34 @@
 const paths = {
 	scripts: {
 		src: 'index.js',
-		dest: '../talis-ui/src/main/content/jcr_root/etc/designs/talis/js'
+		dest: {
+			path: '../talis-ui/src/main/content/jcr_root/etc/designs/talis/js',
+			fileName: 'build.js',
+		}
 	}
 };
 
-module.exports = { paths: paths };
+const webpackConfig = {
+  entry: `./${paths.scripts.src}`,
+	output: {
+		path: '/Users/35267/src/icfolson/aem-web-components/talis/talis-ui/src/main/content/jcr_root/etc/designs/talis/js',
+		filename: "build.js"
+	},
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['babel-preset-env'].map(require.resolve) }
+      }]
+    }]
+  },
+  resolve: {
+    modules: ['./node_modules/']
+  },
+	mode: 'production',
+  devtool: 'source-map'
+};
+
+module.exports = { paths: paths, webpack: webpackConfig };
--- talis-frontend/gulpfile.js
+++ talis-frontend/gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const config = require('./build.config.js');
+const del = require('del');
+const webpack = require('webpack');
 
-gulp.task('build-scripts', () => {
-    return gulp.src(config.paths.scripts.src)
-       .pipe(concat('build.js'))
-       .pipe(gulp.dest(config.paths.scripts.dest));
+gulp.task('clean-scripts', () => {
+    const dest = config.paths.scripts.dest;
+    del([ dest.path + "/" + dest.fileName ], {force: true})
+});
+
+gulp.task('build-scripts', ['clean-scripts'], done => {
+  webpack(config.webpack, (err, stats) => {
+    console.log(err ? err : stats.toString());
+    done();
+  });
 });
 
 gulp.task('default', ['build-scripts']);
